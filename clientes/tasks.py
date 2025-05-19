9c59298fb9cff2f5cec75da115fe75eaa1e94962
IIpapuII <wnserranoa@unadvirtual.edu.co>
2025-05-19 11:39:30 -0500

    Se realiza el proceso de crear la tarea para enviar el codigo temporar para crear el colaborador de forma automatica

---
+++ clientes/tasks.py
@@ -0,0 +1,29 @@
+from celery import shared_task
+from django.conf import settings
+from .models import CodigoTemporal
+from .correo import enviar_correo_con_template
+import ast
+
+@shared_task
+def generar_enviar_codigo_temporal():
+    """
+    Genera un código temporal y lo envía al area correspondiente.
+    """
+    # Generar el código temporal
+    try:
+        codigo_temporal = CodigoTemporal.objects.create()
+        enviar_correo_con_template(
+            asunto="Código Temporal Generado",
+            destinatario= ast.literal_eval(settings.EMAIL_CODIGO_COLABORADOR),
+            template_path='codigo_temporal.html',
+            contexto={
+                'codigo': codigo_temporal.codigo,
+                'fecha_expiracion':codigo_temporal.fecha_vencimiento,
+            }
+        )
+        # Aquí puedes agregar la lógica para enviar el código al área correspondiente
+        # Por ejemplo, enviar un correo electrónico o una notificación
+        print(f"Código temporal generado: {codigo_temporal.codigo}")
+    except Exception as e:
+        print(f"Error al generar o enviar el código temporal: {e}")
+    
